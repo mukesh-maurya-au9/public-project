9fb39016f2545a3858ddc8d7b8daddc1b3b28eae
Mukesh Maurya <mukesh.india9@gmail.com>
2021-02-07 20:12:39 +0530

    setup to Hotel-app

---
+++ hotel-app/src/components/App.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const App = () =>{
+    return (
+        <div className="app">
+            <h1>Welcme @ New Hotel-app</h1>
+        </div>
+    )
+};
+export default App;
